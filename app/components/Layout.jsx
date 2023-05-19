6e9b976bc115f0c71bdb403603163f5c5e05df36
kishan <kishan@cmsminds.com>
2023-05-19 18:40:30 +0530

    kk update

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -27,21 +27,9 @@ import {useCartFetchers} from '~/hooks/useCartFetchers';
 export function Layout({children, layout}) {
   return (
     <>
-      <div className="flex flex-col min-h-screen">
-        <div className="">
-          <a href="#mainContent" className="sr-only">
-            Skip to content
-          </a>
-        </div>
-        <Header
-          title={layout?.shop.name ?? 'Hydrogen'}
-          menu={layout?.headerMenu}
-        />
-        <main role="main" id="mainContent" className="flex-grow">
+      
           {children}
-        </main>
-      </div>
-      <Footer menu={layout?.footerMenu} />
+        
     </>
   );
 }
