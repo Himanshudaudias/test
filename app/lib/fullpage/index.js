6d89729ee25584cba5649b976e237f74245ca661
kishan <kishan@cmsminds.com>
2023-05-19 20:03:09 +0530

    kk update

--- app/lib/fullpage/index.js
+++
@@ -1,9 +0,0 @@
-import Fullpage from './components/Fullpage';
-import Slide from './components/Slide';
-import HorizontalSlider from './components/HorizontalSlider';
-
-export {
-  Fullpage,
-  Slide,
-  HorizontalSlider
-};
