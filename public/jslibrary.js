b61621696936d1317468435b5dad0164a27c6f23
kishan <kishan@cmsminds.com>
2023-06-27 16:48:49 +0530

    updated the files

--- public/jslibrary.js
+++ public/jslibrary.js
@@ -5,33 +5,43 @@ let currentPageIndex = 0;
 let isScrolling = false;
 
 function scrollToPage(index) {
-  if (index < 0 || index >= pages.length || isScrolling) return;
+    if (index < 0 || index >= pages.length || isScrolling) return;
 
-  isScrolling = true;
-  container.scrollTo({
-    top: pages[index].offsetTop,
-    behavior: 'smooth'
-  });
+    isScrolling = true;
+    container.scrollTo({
+        top: pages[index].offsetTop,
+        behavior: 'smooth'
+    });
 
-  setTimeout(() => {
-    isScrolling = false;
-  }, 1000);
+    setTimeout(() => {
+        isScrolling = false;
+    }, 1000);
+
+    // Add active class to the current page
+    pages[currentPageIndex].classList.remove('active');
+    pages[index].classList.add('active');
+    currentPageIndex = index;
 }
 
 document.addEventListener('wheel', (event) => {
-  const delta = Math.sign(event.deltaY);
+    const delta = Math.sign(event.deltaY);
 
-  if (delta > 0) {
-    scrollToPage(currentPageIndex + 1);
-  } else if (delta < 0) {
-    scrollToPage(currentPageIndex - 1);
-  }
+    if (delta > 0) {
+        scrollToPage(currentPageIndex + 1);
+    } else if (delta < 0) {
+        scrollToPage(currentPageIndex - 1);
+    }
 });
 
 container.addEventListener('scroll', () => {
-  const currentPage = Math.round(container.scrollTop / window.innerHeight);
+    const currentPage = Math.round(container.scrollTop / window.innerHeight);
 
-  if (currentPage !== currentPageIndex) {
-    currentPageIndex = currentPage;
-  }
+    if (currentPage !== currentPageIndex) {
+        // Remove active class from the previous page
+        pages[currentPageIndex].classList.remove('active');
+
+        // Add active class to the current page
+        pages[currentPage].classList.add('active');
+        currentPageIndex = currentPage;
+    }
 });
