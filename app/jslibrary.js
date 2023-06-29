76ca4d7684e51eb01490b7e45396cf14063aa265
kishan <kishan@cmsminds.com>
2023-06-29 19:32:03 +0530

    updated the css file

--- app/jslibrary.js
+++ app/jslibrary.js
@@ -5,33 +5,63 @@ let currentPageIndex = 0;
 let isScrolling = false;
 
 function scrollToPage(index) {
-  if (index < 0 || index >= pages.length || isScrolling) return;
+    if (index < 0 || index >= pages.length || isScrolling) return;
 
-  isScrolling = true;
-  container.scrollTo({
-    top: pages[index].offsetTop,
-    behavior: 'smooth'
-  });
+    isScrolling = true;
+    /* container.scrollTo({
+         top: pages[index].offsetTop,
+         behavior: 'smooth'
+     });*/
 
-  setTimeout(() => {
-    isScrolling = false;
-  }, 1000);
+    setTimeout(() => {
+        isScrolling = false;
+    }, 100);
+
+    // Add active class to the current page
+    pages[currentPageIndex].classList.remove('active');
+    pages[index].classList.add('active');
+    currentPageIndex = index;
+}
+
+function handleScroll(delta) {
+    if (delta > 0) {
+        scrollToPage(currentPageIndex + 1);
+    } else if (delta < 0) {
+        scrollToPage(currentPageIndex - 1);
+    }
+}
+
+function handleTouchStart(event) {
+    touchStartY = event.touches[0].clientY;
+}
+
+function handleTouchMove(event) {
+    event.preventDefault();
+    const touchEndY = event.touches[0].clientY;
+    const deltaY = touchEndY - touchStartY;
+
+    handleScroll(Math.sign(deltaY));
 }
 
 document.addEventListener('wheel', (event) => {
-  const delta = Math.sign(event.deltaY);
-
-  if (delta > 0) {
-    scrollToPage(currentPageIndex + 1);
-  } else if (delta < 0) {
-    scrollToPage(currentPageIndex - 1);
-  }
+    const delta = Math.sign(event.deltaY);
+    handleScroll(delta);
 });
 
 container.addEventListener('scroll', () => {
-  const currentPage = Math.round(container.scrollTop / window.innerHeight);
+    const currentPage = Math.round(container.scrollTop / window.innerHeight);
 
-  if (currentPage !== currentPageIndex) {
-    currentPageIndex = currentPage;
-  }
+    if (currentPage !== currentPageIndex) {
+        // Remove active class from the previous page
+        pages[currentPageIndex].classList.remove('active');
+
+        // Add active class to the current page
+        pages[currentPage].classList.add('active');
+        currentPageIndex = currentPage;
+    }
 });
+
+let touchStartY = 0;
+
+container.addEventListener('touchstart', handleTouchStart);
+container.addEventListener('touchmove', handleTouchMove);
