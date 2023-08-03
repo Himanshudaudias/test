2fb97f1f4bfb05dedbe4a736763022fbb46b92c9
Himanshu Daudia <himanshu.daudia@rdisrv.com>
2023-08-03 19:00:46 +0530

    landing page

--- app/components/index.js
+++ app/components/index.js
@@ -23,5 +23,6 @@ export {Grid} from './Grid';
 export {FeaturedProducts} from './FeaturedProducts';
 export {Pagination, getPaginationVariables, usePagination} from './Pagination';
 export {AddToCartButton} from './AddToCartButton';
+export {FullPageScroll} from './FullPageScroll';
 // Sue me
 export * from './Icon';
